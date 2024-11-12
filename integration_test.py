3c3acfee03226936d5dce0a750fd2ecc54a34cf7
Marcel Hernandez <e6990620@gmail.com>
2024-11-12 14:07:34 -0300

    use trustedcoin in integration test, and fix estimatefees and bitcoin-datadir errors

--- integration_test.py
+++ integration_test.py
@@ -1,51 +1,31 @@
 from pyln.client import RpcError
 from pyln.testing.fixtures import *
-from pyln.testing.utils import wait_for
 
 def test_bcli(node_factory, bitcoind, chainparams):
     """
-    This tests the bcli plugin, used to gather Bitcoin data from a local
-    bitcoind.
-    Mostly sanity checks of the interface...
+    Based on the test_bcli from Core Lightning
     """
-
-    l1 = node_factory.get_node()
-    l2 = node_factory.get_node()
+    node = node_factory.get_node(opts={
+        "disable-plugin": "bcli",
+        "plugin": os.path.join(os.getcwd(), 'trustedcoin'),
+    })
 
     # We cant stop it dynamically
     with pytest.raises(RpcError):
-        l1.rpc.plugin_stop("bcli")
+        node.rpc.plugin_stop("bcli")
 
     # Failure case of feerate is tested in test_misc.py
-    estimates = l1.rpc.call("estimatefees")
+    estimates = node.rpc.call("estimatefees")
     assert 'feerate_floor' in estimates
     assert [f['blocks'] for f in estimates['feerates']] == [2, 6, 12, 100]
 
-    resp = l1.rpc.call("getchaininfo", {"last_height": 0})
+    resp = node.rpc.call("getchaininfo", {"last_height": 0})
     assert resp["chain"] == chainparams['name']
     for field in ["headercount", "blockcount", "ibd"]:
         assert field in resp
 
     # We shouldn't get upset if we ask for an unknown-yet block
-    resp = l1.rpc.call("getrawblockbyheight", {"height": 500})
+    resp = node.rpc.call("getrawblockbyheight", {"height": 500})
     assert resp["blockhash"] is resp["block"] is None
-    resp = l1.rpc.call("getrawblockbyheight", {"height": 50})
+    resp = node.rpc.call("getrawblockbyheight", {"height": 50})
     assert resp["blockhash"] is not None and resp["blockhash"] is not None
-    # Some other bitcoind-failure cases for this call are covered in
-    # tests/test_misc.py
-
-    l1.fundwallet(10**5)
-    l1.connect(l2)
-    fc = l1.rpc.fundchannel(l2.info["id"], 10**4 * 3)
-    txo = l1.rpc.call("getutxout", {"txid": fc['txid'], "vout": fc['outnum']})
-    assert (Millisatoshi(txo["amount"]) == Millisatoshi(10**4 * 3 * 10**3)
-            and txo["script"].startswith("0020"))
-    l1.rpc.close(l2.info["id"])
-    # When output is spent, it should give us null !
-    wait_for(lambda: l1.rpc.call("getutxout", {
-        "txid": fc['txid'],
-        "vout": fc['outnum']
-    })['amount'] is None)
-
-    resp = l1.rpc.call("sendrawtransaction", {"tx": "dummy", "allowhighfees": False})
-    assert not resp["success"] and "decode failed" in resp["errmsg"]
--- main_test.go
+++ main_test.go
@@ -11,7 +11,7 @@ import (
 const executable = "./trustedcoin"
 
 const getManifestRequest = `{"jsonrpc":"2.0","id":"getmanifest","method":"getmanifest","params":{}}`
-const getManifestExpectedResponse = `{"jsonrpc":"2.0","id":"getmanifest","result":{"options":[{"name":"bitcoin-rpcconnect","type":"string","default":"","description":"Hostname (IP) to bitcoind RPC (optional)."},{"name":"bitcoin-rpcport","type":"string","default":"","description":"Port to bitcoind RPC (optional)."},{"name":"bitcoin-rpcuser","type":"string","default":"","description":"Username to bitcoind RPC (optional)."},{"name":"bitcoin-rpcpassword","type":"string","default":"","description":"Password to bitcoind RPC (optional)."}],"rpcmethods":[{"name":"getrawblockbyheight","usage":"height","description":"Get the bitcoin block at a given height","long_description":""},{"name":"getchaininfo","usage":"","description":"Get the chain id, the header count, the block count and whether this is IBD.","long_description":""},{"name":"estimatefees","usage":"","description":"Get the Bitcoin feerate in sat/kilo-vbyte.","long_description":""},{"name":"sendrawtransaction","usage":"tx","description":"Send a raw transaction to the Bitcoin network.","long_description":""},{"name":"getutxout","usage":"txid vout","description":"Get informations about an output, identified by a {txid} an a {vout}","long_description":""}],"subscriptions":[],"hooks":[],"featurebits":{"features":"","channel":"","init":"","invoice":""},"dynamic":false,"notifications":[]}}`
+const getManifestExpectedResponse = `{"jsonrpc":"2.0","id":"getmanifest","result":{"options":[{"name":"bitcoin-rpcconnect","type":"string","default":"","description":"Hostname (IP) to bitcoind RPC (optional)."},{"name":"bitcoin-rpcport","type":"string","default":"","description":"Port to bitcoind RPC (optional)."},{"name":"bitcoin-rpcuser","type":"string","default":"","description":"Username to bitcoind RPC (optional)."},{"name":"bitcoin-rpcpassword","type":"string","default":"","description":"Password to bitcoind RPC (optional)."},{"name":"bitcoin-datadir","type":"string","default":"","description":"-datadir arg for bitcoin-cli. For compatibility with bcli, not actually used."}],"rpcmethods":[{"name":"getrawblockbyheight","usage":"height","description":"Get the bitcoin block at a given height","long_description":""},{"name":"getchaininfo","usage":"","description":"Get the chain id, the header count, the block count and whether this is IBD.","long_description":""},{"name":"estimatefees","usage":"","description":"Get the Bitcoin feerate in sat/kilo-vbyte.","long_description":""},{"name":"sendrawtransaction","usage":"tx","description":"Send a raw transaction to the Bitcoin network.","long_description":""},{"name":"getutxout","usage":"txid vout","description":"Get informations about an output, identified by a {txid} an a {vout}","long_description":""}],"subscriptions":[],"hooks":[],"featurebits":{"features":"","channel":"","init":"","invoice":""},"dynamic":false,"notifications":[]}}`
 
 const initRequest = `{"jsonrpc":"2.0","id":"init","method":"init","params":{"options":{},"configuration":{"network":"bitcoin","lightning-dir":"/tmp","rpc-file":"foo"}}}`
 const initExpectedResponse = `{"jsonrpc":"2.0","id":"init"}`
@@ -36,12 +36,12 @@ func start(t *testing.T) (*exec.Cmd, io.WriteCloser, io.ReadCloser, io.ReadClose
 
 	_, _ = io.WriteString(stdin, getManifestRequest)
 	if response := readline(stdout); response != getManifestExpectedResponse {
-		t.Fatalf("unexpected RPC response: %s", response)
+		t.Fatalf("unexpected manifest response: %s", response)
 	}
 
 	_, _ = io.WriteString(stdin, initRequest)
 	if response := readline(stdout); response != initExpectedResponse {
-		t.Fatalf("unexpected RPC response: %s", response)
+		t.Fatalf("unexpected init response: %s", response)
 	}
 
 	if response := readline(stderr); !strings.Contains(response, "initialized plugin") {
