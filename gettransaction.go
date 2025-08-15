d06912c9c865fc08800e739d061f8786bec2b56d
fiatjaf <fiatjaf@gmail.com>
2025-08-15 07:39:18 -0300

    fix gettransaction when explorers return a bad status code.

--- gettransaction.go
+++ gettransaction.go
@@ -56,6 +56,17 @@ func getTransaction(txid string) (tx TxResponse, err error) {
 		}
 		defer w.Body.Close()
 
+		if w.StatusCode >= 400 {
+			data := make([]byte, 99)
+			n, _ := w.Body.Read(data)
+			message := string(data[0:n])
+			if n >= 99 {
+				message += "…"
+			}
+			err = fmt.Errorf("unexpected response: '%s'", message)
+			continue
+		}
+
 		errW = json.NewDecoder(w.Body).Decode(&tx)
 		if errW != nil {
 			err = errW
